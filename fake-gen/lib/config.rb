add6bc5fcf2e8baa057e9143bebf4ff769a15ed5
Yuki Matsuo <yukimaterrace@gmail.com>
2023-05-17 12:09:02 +0900

    Fix: bug

--- fake-gen/lib/config.rb
+++ fake-gen/lib/config.rb
@@ -5,11 +5,11 @@ require 'yaml'
 
 class Config
   def self.load
-    @config = YAML.load_file('./resource/config.yaml')
+    @@config = YAML.load_file('./resource/config.yaml')
     Faker::Config.locale = @@config['faker_locale']
   end
 
   def self.job
-    @config['job']
+    @@config['job']
   end
 end
